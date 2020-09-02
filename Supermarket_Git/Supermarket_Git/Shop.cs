dd42d7ef2bd084640664f74f82567ac11a194ac5
Олександр Owl <aischenko99@gmail.com>
2020-09-02 21:02:05 +0300

    Create class shop and add first part of code

---
+++ Supermarket_Git/Supermarket_Git/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Git
+{
+    class Shop
+    {
+
+        Shelf smallShelf = new Shelf();
+        Shelf middleShelf = new Shelf();
+        Shelf largeShelf = new Shelf();
+        private Statistic _statistic = new Statistic();
+        private int _iteration = 0;
+    }
+}
