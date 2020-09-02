ae7c80b062ffb40dd2a39688c4cf23de3b5a93ab
vohony1 <vohony1@gmail.com>
2020-09-02 21:40:04 +0300

    Added Product class

---
+++ Supermarket_Git/Supermarket_Git/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Supermarket_Git
+{
+    class Product
+    {
+        public string Name;
+        public string Size;
+        public int Amount;
+        public DateTime ExpirationDate;
+        public int Price;
+
+        public Product(string Name, string Size, int Amount, int Price, DateTime ExpirationDate)
+        {
+            this.Name = Name;
+            this.Size = Size;
+            this.Amount = Amount;
+            this.Price = Price;
+            this.ExpirationDate = ExpirationDate;
+        }
+    }
+}
