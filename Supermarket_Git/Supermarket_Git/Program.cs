a31bd410f979880fdde56d5cec605928b36fd315
Олександр Owl <aischenko99@gmail.com>
2020-09-02 20:43:28 +0300

    Create programm

---
+++ Supermarket_Git/Supermarket_Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
