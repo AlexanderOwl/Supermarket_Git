300aed166c01893e3cc751fa8723c313e33da2bd
Олександр Owl <aischenko99@gmail.com>
2020-09-03 11:20:40 +0300

    Add logic in class programm

--- Supermarket_Git/Supermarket_Git/Program.cs
+++ Supermarket_Git/Supermarket_Git/Program.cs
@@ -8,8 +8,22 @@ namespace Supermarket_Git
 {
     class Program
     {
+        static DateTime date = new DateTime();
         static void Main(string[] args)
         {
+            Shop shop = new Shop();
+            List<Product> productsInShop = new List<Product>();
+            List<Customer> customers = new List<Customer>();
+            date = DateTime.Today;
+
+            if (Password.askpass())
+            {
+                do
+                {
+                    productsInShop = shop.Menu(productsInShop, ref date, customers);
+                    Console.Clear();
+                } while (true);
+            }
         }
     }
 }
