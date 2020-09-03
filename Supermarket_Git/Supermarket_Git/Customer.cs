0c137477dd114f80e95b7c03f5825fe13aca110d
DESKTOP-1VA8JIU\msotn <m.sotnikov1986@gmail.com>
2020-09-03 12:03:57 +0300

    Created class Customer

---
+++ Supermarket_Git/Supermarket_Git/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_Git
+{
+    class Customer
+    {
+        public List<Product> ProductsList = new List<Product>();
+        public int Cash;
+        public int QueueNumber;
+        public string Name;
+        private Storage _storage = new Storage();
+
+        public Customer(string Name, int QueueNumber)
+        {
+            this.QueueNumber = QueueNumber;
+            this.Name = Name;
+            this.ProductsList = ShopingListGenerator(_storage.ProductGenerator());
+            this.Cash = AmountGenerator(20, 1000);
+        }
+
+    }
+}
